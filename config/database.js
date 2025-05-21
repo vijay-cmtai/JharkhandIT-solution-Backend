b123b2b85469262b96dbab0099e5bde0b2ec6c4e
vjzest <126171677+vjzest@users.noreply.github.com>
2025-05-21 17:25:26 +0530

    backend

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = { connect };
